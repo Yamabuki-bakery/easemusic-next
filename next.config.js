875ea695a2b826aa8d8c5a0827b7c07658202487
Yukina <66236255+Yamabuki-bakery@users.noreply.github.com>
2022-11-02 00:09:58 +0900

    砍掉了 edge server，不然 router 會崩

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    runtime: 'experimental-edge', // 'node.js' (default) | experimental-edge
-  },
+  // experimental: {
+  //   runtime: 'experimental-edge', // 'node.js' (default) | experimental-edge
+  // },
 }
 
 module.exports = nextConfig
